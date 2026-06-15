785f8a9c80a3379a2696bae1baa5e20bb5a5c253
Roman Knyazev <i88ronin@icloud.com>
2026-06-15 18:32:04 +0700

    add structure

---
+++ main_logic.py
@@ -0,0 +1,4 @@
+while True:
+    print("=== TASK MANAGER ===")
+    user_input = input("Enter your choice\n1 - See tasks\n2 - Update tasks\n3 - Change tasks\n4 - Delete tasks\n5 - About this app\n6 - Exit")
+
---
+++ task.py
@@ -0,0 +1,5 @@
+class Task:
+    def __init__(self, title, status) -> None:
+        self.title = title
+        self.status = status
+        
