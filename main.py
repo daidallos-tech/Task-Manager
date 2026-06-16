2cded3e620b8aecbad1fbfffe7c694a8d202f41d
Roman Knyazev <i88ronin@icloud.com>
2026-06-16 21:53:03 +0700

    first release

---
+++ main.py
@@ -0,0 +1,5 @@
+from main_logic import main_logic
+
+if __name__ == "__main__":
+    main_logic()
+    
--- main_logic.py
+++ main_logic.py
@@ -1,4 +1,28 @@
-while True:
-    print("=== TASK MANAGER ===")
-    user_input = input("Enter your choice\n1 - See tasks\n2 - Update tasks\n3 - Change tasks\n4 - Delete tasks\n5 - About this app\n6 - Exit")
+from task import Task, TaskManager
+
+
+manager = TaskManager()
+def main_logic():
+    while True:
+        print("=== TASK MANAGER ===")
+        user_input = input("Enter your choice\n1 - Create task\n2 - See task(s)\n3 - Exit\nEnter: ")
+
+        if user_input == "1":
+            title_input = input("Enter a title of your task: ")
+            description_input = input("Enter a description of your task: ")
+            status_input = input("Enter a status of your task: ")
+
+            try:
+                new_task = Task(title=title_input, description=description_input, status=status_input)
+                manager.add_task(new_task)
+                continue
+            except ValueError as e:
+                print(f"Error: {e}. Try again!")
+        elif user_input == "2":
+            manager.show_tasks()
+        elif user_input =="3":
+            print("Close app...")
+            break
+        else:
+            print("Invalid choice. Try again!")
 
